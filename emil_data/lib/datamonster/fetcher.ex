562c3759701c70c0288798838b415f39a562c861
Victoria Wagman <victoriawagman@gmail.com>
2017-03-11 21:40:17 +0100

    NOT WORKING - sth wrong when fetching resources - taking to long? Not written to file...

--- emil_data/lib/datamonster/fetcher.ex
+++ emil_data/lib/datamonster/fetcher.ex
@@ -1,8 +1,8 @@
 defmodule Datamonster.Fetcher do
   def fetch() do
     HTTPoison.start
-    response = HTTPoison.get! "http://victoriawagman.com"
-    xhtml = response.body
-    File.write! "./educational-resources-from-emil.xml", xhtml
+    response = HTTPoison.get! "http://supernavet.skolverket.se/SusaNavExport/EmilExporter"
+    xml = response.body
+    File.write! "./educational-resources-from-emil.xml", xml
   end
 end
